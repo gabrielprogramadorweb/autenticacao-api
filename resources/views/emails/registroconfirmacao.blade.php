28eff5f30e17841fed29962582905729e21c2c37
Gabriel Castro <gabriel.programador.ads@gmail.com>
2023-12-22 21:00:37 -0300

    first commit

---
+++ resources/views/emails/registroconfirmacao.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <h4>Seja bem vindo(a), {{$nome}} </h4>
+        <p>Você acabou de acessar o sistema utilizando seu email: {{$email}} </p>
+        <p>Data / hora de acesso: {{$datahora}} </p>
+        <p>Clique no link abaixo para confirmar seu email de registro:</p>
+
+        <a href="{{$link}}">Clique aqui</a>
+    </body>
+</html>
